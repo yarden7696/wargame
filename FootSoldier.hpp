4060888be2fe3e2e00afc637e104d4c3519239ed
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2020-06-27 20:37:48 +0300

    Update FootSoldier.hpp

--- FootSoldier.hpp
+++ FootSoldier.hpp
@@ -2,14 +2,15 @@
 #include "Soldier.hpp"
 using namespace std;
 
-
 class FootSoldier : public Soldier {
     public:
     FootSoldier (int solider_number) :
-            Soldier(100, -10, solider_number,100){}
+            Soldier(100, -10, solider_number,100){}    // 100 = max_health
+                                                      // -10 = demage
+                                                     // solider_number = which group
+                                                    // 100 = current_health
 
         void playS (std::vector<std::vector<Soldier*>> &board, pair<int,int> location);
-
 
 };
 
